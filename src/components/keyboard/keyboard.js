16243b329da4b282bbcf311b28605e617354d04d
dragonflyinmypool <65822494+dragonflyinmypool@users.noreply.github.com>
2020-10-12 17:00:39 -0400

    folder reapeared

--- src/components/keyboard/keyboard.js
+++ src/components/keyboard/keyboard.js
@@ -6,18 +6,28 @@ const Keyboard = ({ keyboard, pickedLetters, keyClick }) => {
     let b;
 
     if (pickedLetters.includes(a)) {
-      b = "";
+      b = ".";
     } else {
       b = a;
     }
     return b;
   });
   console.log(updatedKeyboard);
-  let keyboardHtml = updatedKeyboard.map((a, b) => (
-    <button onClick={keyClick.bind(a)} className="key" key={b}>
-      {a}
-    </button>
-  ));
+  let keyboardHtml = updatedKeyboard.map(function (a, b) {
+    if (a === ".") {
+      return (
+        <button className="keyclicked" key={b}>
+          {a}
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={keyClick.bind(a)} className="key" key={b}>
+          {a}
+        </button>
+      );
+    }
+  });
 
   return <div className="keyboard">{keyboardHtml}</div>;
 };
